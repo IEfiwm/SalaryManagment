453605e1391d4f5c2da0b67dc912ee3dfadfd941
IEfiwm <68849376+IEfiwm@users.noreply.github.com>
2022-01-05 22:18:48 +0330

    debug

--- Infrastructure/Identity/Seeds/DefaultBasicUser.cs
+++ Infrastructure/Identity/Seeds/DefaultBasicUser.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Enums;
 using Domain.Entities.Base.Identity;
 using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,7 +12,8 @@ namespace Infrastructure.Identity.Seeds
         public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             //Seed Default User
-            var defaultUser = new ApplicationUser
+
+            var adminUser = new ApplicationUser
             {
                 UserName = "user",
                 Email = "user@gmail.com",
@@ -21,7 +23,20 @@ namespace Infrastructure.Identity.Seeds
                 PhoneNumberConfirmed = true,
                 IsActive = true
             };
-            var tjUser = new ApplicationUser
+
+            if (userManager.Users.All(u => u.Id != adminUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(adminUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(adminUser, "123Pa$$word!");
+                    await userManager.AddToRoleAsync(adminUser, Roles.Admin.ToString());
+                }
+            }
+
+            var userList = new List<ApplicationUser>();
+
+            userList.Add(new ApplicationUser
             {
                 UserName = "tjadmin",
                 Email = "tjadmin@gmail.com",
@@ -30,21 +45,74 @@ namespace Infrastructure.Identity.Seeds
                 EmailConfirmed = true,
                 PhoneNumberConfirmed = true,
                 IsActive = true
-            };
-            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            });
+
+            userList.Add(new ApplicationUser
             {
-                var user = await userManager.FindByEmailAsync(defaultUser.Email);
-                if (user == null)
+                UserName = "bashiri",
+                Email = "bashiri@gmail.com",
+                FirstName = "",
+                LastName = "Bashiri",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                IsActive = true
+            });
+
+            userList.Add(new ApplicationUser
+            {
+                UserName = "eftekhari",
+                Email = "eftekhari@gmail.com",
+                FirstName = "",
+                LastName = "Eftekhari",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                IsActive = true
+            });
+
+            userList.Add(new ApplicationUser
+            {
+                UserName = "khorasani",
+                Email = "khorasani@gmail.com",
+                FirstName = "",
+                LastName = "Khorasani",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                IsActive = true
+            });
+
+            userList.Add(new ApplicationUser
+            {
+                UserName = "jabarzadeh",
+                Email = "jabarzadeh@gmail.com",
+                FirstName = "",
+                LastName = "JabarZadeh",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                IsActive = true
+            });
+
+            userList.Add(new ApplicationUser
+            {
+                UserName = "alinaghi",
+                Email = "alinaghi@gmail.com",
+                FirstName = "",
+                LastName = "Alinaghi",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                IsActive = true
+            });
+
+            foreach (var defaultUser in userList)
+            {
+                if (userManager.Users.All(u => u.Id != defaultUser.Id))
                 {
-                    await userManager.CreateAsync(defaultUser, "123Pa$$word!");
-                    await userManager.AddToRoleAsync(defaultUser, Roles.Admin.ToString());
-                }                
-                
-                user = await userManager.FindByEmailAsync(tjUser.Email);
-                if (user == null)
-                {
-                    await userManager.CreateAsync(tjUser, "123Pa$$word!");
-                    await userManager.AddToRoleAsync(tjUser, Roles.Admin.ToString());
+                    var user = await userManager.FindByEmailAsync(defaultUser.Email);
+
+                    if (user == null)
+                    {
+                        await userManager.CreateAsync(defaultUser, "123Pa$$word!");
+                        await userManager.AddToRoleAsync(defaultUser, Roles.Manager.ToString());
+                    }
                 }
             }
         }
