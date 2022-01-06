405315cadba924a73f24ed7385f16373476bc854
IEfiwm <68849376+IEfiwm@users.noreply.github.com>
2022-01-06 12:53:46 +0330

    develop unitofwork

--- Application/Interfaces/Repositories/IUnitOfWork.cs
+++ Application/Interfaces/Repositories/IUnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,5 +10,22 @@ namespace Application.Interfaces.Repositories
         Task<int> Commit(CancellationToken cancellationToken);
 
         Task Rollback();
+
+        /// <summary>
+        /// Executes the specified raw SQL command.
+        /// </summary>
+        /// <param name="sql">The raw SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The number of state entities written to database.</returns>
+        int ExecuteSqlCommand(string sql, params object[] parameters);
+
+        /// <summary>
+        /// Uses raw SQL queries to fetch the specified <typeparamref name="TEntity"/> data.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="sql">The raw SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>An <see cref="IQueryable{T}"/> that contains elements that satisfy the condition specified by raw SQL.</returns>
+        IQueryable<TEntity> FromSql<TEntity>(string sql, params object[] parameters) where TEntity : class;
     }
 }
--- Infrastructure/Repositories/UnitOfWork.cs
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -1,9 +1,12 @@
 ﻿using Application.Interfaces.Repositories;
 using Application.Interfaces.Shared;
 using Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Infrastructure.Repositories
 {
@@ -48,6 +51,47 @@ namespace Infrastructure.Repositories
             }
             //dispose unmanaged resources
             disposed = true;
+        }
+
+        /// <summary>
+        /// Executes the specified raw SQL command.
+        /// </summary>
+        /// <param name="sql">The raw SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The number of state entities written to database.</returns>
+        public int ExecuteSqlCommand(string sql, params object[] parameters) => _dbContext.Database.ExecuteSqlRaw(sql, parameters);
+
+        /// <summary>
+        /// Uses raw SQL queries to fetch the specified <typeparamref name="TEntity" /> data.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="sql">The raw SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>An <see cref="IQueryable{T}" /> that contains elements that satisfy the condition specified by raw SQL.</returns>
+        public IQueryable<TEntity> FromSql<TEntity>(string sql, params object[] parameters) where TEntity : class => _dbContext.Set<TEntity>().FromSqlRaw(sql, parameters);
+
+        /// <summary>
+        /// Saves all changes made in this context to the database with distributed transaction.
+        /// </summary>
+        /// <param name="ensureAutoHistory"><c>True</c> if save changes ensure auto record the change history.</param>
+        /// <param name="unitOfWorks">An optional <see cref="IUnitOfWork"/> array.</param>
+        /// <returns>A <see cref="Task{TResult}"/> that represents the asynchronous save operation. The task result contains the number of state entities written to database.</returns>
+        public async Task<int> SaveChangesAsync(bool ensureAutoHistory = false, params IUnitOfWork[] unitOfWorks)
+        {
+            using (var ts = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var count = 0;
+                foreach (var unitOfWork in unitOfWorks)
+                {
+                    count += await unitOfWork.SaveChangesAsync(ensureAutoHistory).ConfigureAwait(false);
+                }
+
+                count += await SaveChangesAsync(ensureAutoHistory);
+
+                ts.Complete();
+
+                return count;
+            }
         }
     }
 }
