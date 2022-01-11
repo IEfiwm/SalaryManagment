45f5711483852cc578cef511ef3f3331ddc4cb1a
Marzie Keshavarz <marziekeshavarz97@gmail.com>
2022-01-11 22:10:19 +0330

    tax reports

---
+++ Web/Areas/Export/Controllers/TaxController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Web.Abstractions;
+using Web.Controllers;
+
+namespace Web.Areas.Export.Controllers
+{
+    [Area("Export")]
+    [Authorize(Roles = "SuperAdmin,Admin,Manager")]
+    public class TaxController : BaseController<TaxController>
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TXT(int year, int month, List<string> projectList)
+        {
+            var projectId = string.Join(',', projectList);
+            return Redirect(@$"{_configuration["Base:KoshaCore:APIAddress"].ToString()}/Report/TXTTaxAll/{year}/{month}/{projectId}");
+        }
+
+        [HttpPost]
+        public IActionResult PDF(int year, int month, List<string> projectList)
+        {
+            var projectId = string.Join(',', projectList);
+            return Redirect(@$"{_configuration["Base:KoshaCore:APIAddress"].ToString()}/Report/TaxAll/{year}/{month}/{projectId}");
+        }
+    }
+}
---
+++ Web/Areas/Export/Controllers/TaxSummaryInsurance.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Web.Abstractions;
+
+namespace Web.Areas.Export.Controllers
+{
+    [Area("Export")]
+    [Authorize(Roles = "SuperAdmin,Admin,Manager")]
+    public class TaxSummaryInsurance : BaseController<TaxController>
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TXT(int year, int month, List<string> projectList)
+        {
+            var projectId = string.Join(',', projectList);
+            return Redirect(@$"{_configuration["Base:KoshaCore:APIAddress"].ToString()}/Report/TXTTaxSummary/{year}/{month}/{projectId}");
+        }
+
+        [HttpGet]
+        public IActionResult PDF(int year, int month, List<string> projectList)
+        {
+            var projectId = string.Join(',', projectList);
+            return Redirect(@$"{_configuration["Base:KoshaCore:APIAddress"].ToString()}/Report/TaxSummary/{year}/{month}/{projectId}");
+        }
+    }
+}
