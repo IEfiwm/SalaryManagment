971d17131e6416b4178973d4c15ca91f077725a9
IEfiwm <68849376+IEfiwm@users.noreply.github.com>
2022-03-25 00:28:39 +0430

    new dashboard

--- Web/Areas/Dashboard/Controllers/ManagmentController.cs
+++ Web/Areas/Dashboard/Controllers/ManagmentController.cs
@@ -1,16 +1,42 @@
 ﻿using Web.Abstractions;
 using Microsoft.AspNetCore.Mvc;
+using Infrastructure.Repositories.Application.Basic;
+using Infrastructure.Repositories.Application;
+using Infrastructure.Repositories.Application.Idenitity;
+using Web.Areas.Dashboard.Models;
+using System.Threading.Tasks;
 
 namespace Web.Areas.Dashboard.Controllers
 {
     [Area("Dashboard")]
     public class ManagmentController : BaseController<ManagmentController>
     {
-        public IActionResult Index()
+        private readonly IProjectRepository _projectRepository;
+
+        private readonly IimportedRepository _iimportedRepository;
+
+        private readonly IUserRepository _userRepository;
+
+        public ManagmentController(IProjectRepository projectRepository,
+            IimportedRepository iimportedRepository,
+            IUserRepository userRepository)
         {
+            _iimportedRepository = iimportedRepository;
+            _projectRepository = projectRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = new SummaryViewModel();
+
+            model.ImportedDataCount = (await _iimportedRepository.GetListAsync()).Count;
+            model.ProjectCount = (await _projectRepository.GetListAsync()).Count;
+            model.UserCount = (await _userRepository.GetUserListAsync()).Count;
+
             _notify.Information("خوش آمدید !");
-            
-            return View();
+
+            return View(model);
         }
     }
-}   
+}
---
+++ Web/Areas/Dashboard/Models/SummaryViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Areas.Dashboard.Models
+{
+    public class SummaryViewModel
+    {
+        public long ProjectCount { get; set; }
+
+        public long UserCount { get; set; }
+
+        public long ImportedDataCount { get; set; }
+    }
+}
