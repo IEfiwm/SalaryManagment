136021b1cdb2773e1a7bb93dfb45520b246e3ecc
IEfiwm <68849376+IEfiwm@users.noreply.github.com>
2022-04-08 19:50:05 +0430

    Update ApplicationRole

---
+++ Domain/Entities/Base/Identity/ApplicationRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities.Base.Identity
+{
+    public partial class ApplicationRole : IdentityRole
+    {
+    }
+}
--- Infrastructure/Map/Base/Identity/ApplicationRolesMap.cs
+++ Infrastructure/Map/Base/Identity/ApplicationRolesMap.cs
@@ -1,16 +1,16 @@
-﻿using Infrastructure.Attribute;
-using Microsoft.AspNetCore.Identity;
+﻿using Domain.Entities.Base.Identity;
+using Infrastructure.Attribute;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Map.Base.Identity
 {
     [Base]
-    internal class ApplicationRolesMap : IEntityTypeConfiguration<IdentityRole>
+    public class ApplicationRolesMap : IEntityTypeConfiguration<ApplicationRole>
     {
-        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        public void Configure(EntityTypeBuilder<ApplicationRole> builder)
         {
-            builder.ToTable(name: "Roles","Identity");
+            builder.ToTable(name: "Roles", "Identity");
         }
     }
 }
