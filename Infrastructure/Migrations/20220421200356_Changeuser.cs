4b13d88d49d1e79d7e60656ac16af749ed5d7bdb
IEfiwm <68849376+IEfiwm@users.noreply.github.com>
2022-04-22 00:40:13 +0430

    update

---
+++ Infrastructure/Migrations/20220421200356_Changeuser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class Changeuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsInsurance",
+                schema: "Identity",
+                table: "Users");
+
+            //migrationBuilder.RenameTable(
+            //    name: "ApplicationUserApplicationUser",
+            //    newName: "ApplicationUserApplicationUser",
+            //    newSchema: "Identity");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.RenameTable(
+            //    name: "ApplicationUserApplicationUser",
+            //    schema: "Identity",
+            //    newName: "ApplicationUserApplicationUser");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsInsurance",
+                schema: "Identity",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
