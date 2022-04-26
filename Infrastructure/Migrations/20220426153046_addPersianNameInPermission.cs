1745fe1c7937a382265723132518e5926cdf088b
Marzie Keshavarz <marziekeshavarz97@gmail.com>
2022-04-26 20:18:01 +0430

    persian permission name

---
+++ Infrastructure/Migrations/20220426153046_addPersianNameInPermission.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class addPersianNameInPermission : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.RenameTable(
+            //    name: "ApplicationUserApplicationUser",
+            //    newName: "ApplicationUserApplicationUser",
+            //    newSchema: "Identity");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PersianName",
+                schema: "Basic",
+                table: "TbPermission",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersianName",
+                schema: "Basic",
+                table: "TbPermission");
+
+            //migrationBuilder.RenameTable(
+            //    name: "ApplicationUserApplicationUser",
+            //    schema: "Identity",
+            //    newName: "ApplicationUserApplicationUser");
+        }
+    }
+}
