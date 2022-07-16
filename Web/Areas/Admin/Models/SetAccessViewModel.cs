db8fb6b81a84d2484b1ab57f7f5b3e73d09e43df
IEfiwm <68849376+IEfiwm@users.noreply.github.com>
2022-07-16 23:13:30 +0430

    SetPayrollAccess

---
+++ Web/Areas/Admin/Models/SetAccessViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Areas.Admin.Models
+{
+    public class SetAccessViewModel
+    {
+        public int ProjectId { get; set; }
+
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+    }
+}
