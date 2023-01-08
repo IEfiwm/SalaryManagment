13ffcb38b3b1b4ec8ce75fdb203098e8b1025e75
IEfiwm <68849376+IEfiwm@users.noreply.github.com>
2023-01-09 00:35:32 +0330

    debug

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -111,7 +111,7 @@ namespace Web
 
             app.Use(async (context, next) =>
             {
-                if (!(DateTime.Now < new DateTime(2022, 12, 15)))
+                if (!(DateTime.Now < new DateTime(2023, 3, 30)))
                     context.Response.StatusCode = 403;
                 else
                     await next();
